3fcac30caa23845fb9474ae4ed73361d37d320c3
LeMinaw <leminaw@gmail.com>
2018-03-07 02:54:13 +0100

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# walkers
+# Walkers Alpha
+
+Dynamic systems vizualisation software, written in Julia.
+
+## How to run it
+
+### On Windows
+
+Standalone windows builds are located in the `builds` directory of this repo.
+
+*The application takes a few moments to init, please be patient.*
+
+### In Julia's REPL
+
+To run the software in Julia's REPL, please use
+```julia
+>>> include("walkers.jl")
+>>> Walkers.julia_main()
+```
+
+*The first run will take a bit long due to JIT compilation overhead, but
+following ones should take no time. If you hack into the code, it's probably
+more conveniant to load Walkers from the REPL instead of using the `julia`
+command from a system shell then.*
+
+## How to use it
+
+### Quick start
+
+If you don't know where to start and want to experiment by yourself quickly,
+try to modify these parameters.
+
+* **Attraction:**     0.005
+* **Walkers spread:** 100
+* **Iterations:**     100
+* **Walkers number:** 10
+
+Then click the **Center** button.
+
+You can now experiment with the different parameters as you want.
+
+### Manual
+
+#### Abstract
+
+A walker is a point in space. Each walker is bind to another by an attraction
+actor.
+
+At each iteration, walkers walk towards other walkers who attact them, and
+escape from those they flee. Black lines describe walkers' paths in time,
+while color lines represent the figure they make at any time.
+
+#### Parameters
+
+This describes more precisely what controls do.
+
+* **Center:**         Automatically centers and fit the 3D view.
+* **Regenerate:**     Modifies initial conditions by updating the
+    random number seed.
+* **Color map:**      Sets colors for the rings geometry. Four channels are
+    supported, RGB plus alpha (transparency). Left  values are first iterations,
+    right values are last ones.
+* **Variance:**       How random are attraction values. Null means no random.
+* **Attraction:**     Average attraction values that binds walkers together.
+    Negative value means repulsion, null means no relation, positive value is
+    attraction.
+* **Walkers spread:** Average distance walkers have from the origin at start.
+* **Walkers number:** How many walkers the system counts.
+* **Iterations:**     Number of iterations to compute.
+* **Rotation speed:** Automatically rotates the 3D view on the Z axis for a
+    nice screensaver effect.
---
+++ walkers.jl
@@ -0,0 +1,152 @@
+module Walkers
+
+using GLAbstraction, Colors, GeometryTypes, GLVisualize, Reactive, GLWindow
+import GLVisualize: mm, widget, button, slider, labeled_slider
+
+# import Colors: RGBA, colormap
+# import GeometryTypes: Vec2f0, Point3f0
+# import Reactive: Signal, map, value, preserve
+# import GLWindow: Screen
+# import GLAbstraction: rotationmatrix_z
+# import GLVisualize: glscreen, _view, visualize, renderloop, x_partition_abs, loop, center!
+
+function scatter(x::Any, avg=0::Real, width=1::Rand)
+    x * 2width - width + avg
+end
+
+function shufflecols(mat::Array{T, 2}) where T
+    mat[:, randperm(size(mat, 2))]
+end
+
+function offsetcols(mat::Array{T, 2}) where T
+    if size(mat, 2) == 1
+        return mat
+    end
+    perms = collect(2:size(mat, 2))
+    push!(perms, 1)
+    mat[:, perms]
+end
+
+function walk(n, pts, rels)
+    states = Point3f0[]
+    for i = 1:n
+        append!(states, pts)
+        # pts += rels * pts - transpose(transpose(pts) * rels)
+        pts += rels * pts - sum(rels, 2) .* pts
+    end
+    states
+end
+
+Base.@ccallable function julia_main()::Cint # For compilation with PackageCompiler
+    # OpenGL interface init
+    window = glscreen("Walkers Alpha v1.0.0")
+
+    # GUI layout
+    editarea, viewarea = x_partition_abs(window.area, 70mm)
+    editscreen = Screen(
+        window, area = editarea,
+        color = RGBA{Float32}(0.98, 0.98, 0.98, 1)
+    )
+    viewscreen = Screen(window, area=viewarea)
+
+    # GUI parameters
+    speed_gui,      speed_s      = labeled_slider(0:.1:10,                      editscreen)
+    walkers_gui,    walkers_s    = labeled_slider(2:1:40,                       editscreen)
+    iterations_gui, iterations_s = labeled_slider(2:1:1000,                     editscreen)
+    spread_gui,     spread_s     = labeled_slider(0:.1:100,                     editscreen)
+    attrac_gui,     attrac_s     = labeled_slider(-.05:.001:.05,                editscreen)
+    variance_gui,   variance_s   = labeled_slider(0:.001:.1,                    editscreen)
+    cmap_gui,       cmap_s       = widget(RGBA{Float32}.(colormap("Reds", 5)),  editscreen)
+    center_gui,     center_s     = button("⛶",                                  editscreen)
+    regen_gui,      regen_s      = button("↻",                                  editscreen)
+    params = Pair[
+        "Rotation speed" => speed_gui,
+        "Walkers number" => walkers_gui,
+        "Iterations"     => iterations_gui,
+        "Walkers spread" => spread_gui,
+        "Attraction"     => attrac_gui,
+        "Variance"       => variance_gui,
+        "Color map"      => cmap_gui,
+        "Center"         => center_gui,
+        "Regenerate"     => regen_gui
+    ]
+    menu = visualize(params)
+    _view(menu, editscreen, camera=:fixed_pixel)
+
+    # DEBUG
+    # speed_s      = Signal(1)
+    # walkers_s    = Signal(20)
+    # iterations_s = Signal(50)
+    # spread_s     = Signal(40)
+    # attrac_s     = Signal(.001)
+    # variance_s   = Signal(0)
+    # cmap_s       = Signal()
+    # center_s     = Signal(false)
+    # regen_s      = Signal(false)
+
+    # Random number generator initialisation
+    seed_s = Signal(abs(rand(Int32)))
+    rng = MersenneTwister(0)
+
+    # Buttons actions
+    preserve(map(center_s, regen_s) do center, regen # preserve() avoids GC
+        if center
+            center!(viewscreen)
+        elseif regen
+            seed = abs(rand(Int32)) # Uses the global RNG
+            push!(seed_s, seed)
+        end
+        nothing
+    end)
+
+    # Computation and signals mappings
+    time_s = loop(linspace(0f0, Float32(typemax(Int32)), typemax(Int32)))
+    rot_s = map(time_s, speed_s) do t, s
+        rotationmatrix_z(Float32(t * s/360)) # -> 4x4 Float32 rotation matrix
+    end
+
+    points_s = map(walkers_s, spread_s, seed_s) do n, spread, seed
+        srand(rng, seed) # Inits local RNG
+        scatter.(rand(rng, Point3f0, n), 0, spread)
+    end
+    relations_s = map(walkers_s, attrac_s, variance_s, seed_s) do n, attrac, variance, seed
+        srand(rng, seed) # Inits local RNG # scatter.(rand(rng, n, n), attrac, variance)
+        scatter.(offsetcols(eye(n)), attrac, variance)
+    end
+
+    states_s = map(iterations_s, points_s, relations_s) do iters, pts, rels
+        walk(map(value, (iters, pts, rels))...)
+    end
+    rings_s = map(states_s, iterations_s, walkers_s) do states, iters, wlkrs
+        reshape(states, wlkrs, iters)
+    end
+    lines_s = map(rings_s) do states
+        transpose(states)
+    end
+    cnorm_s = map(iterations_s) do iters
+        Vec2f0(1, iters/1024)
+    end
+
+    # Plotting
+    lines = visualize(
+        lines_s, :lines,
+        intensity = [0f0, 0f0],
+        color_map = [RGBA{Float32}(0, 0, 0, 0.8), RGBA{Float32}(0, 0, 0, 0.8)],
+        color_norm = Vec2f0(0, 1),
+        model = rot_s,
+    )
+    rings = visualize(
+        rings_s, :lines,
+        intensity = collect(linspace(0f0, 1f0, 1024)),
+        color_map = cmap_s,
+        color_norm = cnorm_s,
+        model = rot_s,
+    )
+    _view(lines, viewscreen, camera=:perspective)
+    _view(rings, viewscreen, camera=:perspective)
+
+    renderloop(window)
+    return 0
+end
+
+end
