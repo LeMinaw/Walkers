dfbdeae3a24a9c4bd4dd1d90b8ea774ed9797873
LeMinaw <leminaw@gmail.com>
2018-03-13 16:43:50 +0100

    multiple relation and dynamics models

--- walkers.jl
+++ walkers.jl
@@ -10,20 +10,26 @@ import Base./
 # import GLAbstraction: rotationmatrix_z
 # import GLVisualize: glscreen, _view, visualize, renderloop, x_partition_abs, loop, center!
 
+const version = "1.0.0"
+
+@enum Laws position=0 acceleration=1 newton=2
+
+@enum Relations onetoone=0 manytomany=1 electronic=2
+
 function /(k::Int, pt::Point{})
     k ./ pt
 end
 
-function replacenan!{T}(mat::Array{T})
+function replacenan!(mat::Array{T}) where T
     mat[isnan(x)] = zero(T)
 end
 
-function nulldiagonal{T}(mat::Array{T})
+function nulldiag(mat::Array)
     triu(mat, 1) + tril(mat, -1)
 end
 
-function normalize{T}(mat::Array{T})
-    r = nulldiagonal(mat ./ norm.(mat))
+function normalize(mat::Array)
+    mat ./ norm.(mat)
 end
 
 function scatter(x::Any, avg=0::Real, width=1::Rand)
@@ -39,31 +45,43 @@ function offsetcols(mat::Array{T, 2}) where T
         return mat
     end
     perms = collect(2:size(mat, 2))
-    push!(perms, 1)
+        push!(perms, 1)
     mat[:, perms]
 end
 
-function walk(n, pos, rels)
+function diffs(mat::Array)
+    dupl = repmat(mat, 1, size(mat, 1))
+    transpose(dupl) - dupl
+end
+
+function walk(law::Laws, n::Int, pos::Array, rels::Array)
     states = Point3f0[]
     vel = fill(Vec3f0(0, 0, 0), length(pos))
     for i = 1:n
         append!(states, pos)
+        # TODO: Experiment again with this relation:
         # pos += rels * pos - transpose(transpose(pos) * rels)
-        # pos += rels * pos - sum(rels, 2) .* pos
-        # d = norm.(100rels * pos - sum(100rels, 2) .* pos)
-        dist = repmat(pos, 1, size(pos, 1))
-        dist = transpose(dist) - dist
-        acc = sum(10rels .* (normalize(dist)), 2)
-
-        vel += acc
-        pos += vel
+        # TODO: Add option to normalize, eg:
+        # acc = sum(10rels .* (nulldiag(normalize(dist))), 2)
+        if law == position
+            # Increases position by a part of the distance
+            pos += rels * pos - sum(rels, 2) .* pos
+        else
+            dist = diffs(pos)
+            if law == newton
+                # Newton's and Coulomb's law is of form k/d²
+                dist = 100^2 * nulldiag(normalize(dist) .* norm.(dist) .^ -1)
+            end
+            vel += sum(.1rels .* dist, 2)
+            pos += vel
+        end
     end
     states
 end
 
 Base.@ccallable function app()::Cint # For compilation with PackageCompiler
     # OpenGL interface init
-    window = glscreen("Walkers Alpha v1.0.0")
+    window = glscreen("Walkers Alpha v$(version)")
 
     # GUI layout
     editarea, viewarea = x_partition_abs(window.area, 70mm)
@@ -81,6 +99,8 @@ Base.@ccallable function app()::Cint # For compilation with PackageCompiler
     attrac_gui,     attrac_s     = labeled_slider(-.05:.001:.05,                editscreen)
     variance_gui,   variance_s   = labeled_slider(0:.001:.1,                    editscreen)
     cmap_gui,       cmap_s       = widget(RGBA{Float32}.(colormap("Reds", 5)),  editscreen)
+    law_gui,        law_s        = widget(Signal(position),                     editscreen)
+    relation_gui,   relation_s   = widget(Signal(onetoone),                     editscreen)
     center_gui,     center_s     = button("⛶",                                  editscreen)
     regen_gui,      regen_s      = button("↻",                                  editscreen)
     params = Pair[
@@ -91,6 +111,8 @@ Base.@ccallable function app()::Cint # For compilation with PackageCompiler
         "Attraction"     => attrac_gui,
         "Variance"       => variance_gui,
         "Color map"      => cmap_gui,
+        "Relation model" => relation_gui,
+        "Dynamics law"   => law_gui,
         "Center"         => center_gui,
         "Regenerate"     => regen_gui
     ]
@@ -118,7 +140,7 @@ Base.@ccallable function app()::Cint # For compilation with PackageCompiler
             center!(viewscreen)
         elseif regen
             seed = abs(rand(Int32)) # Uses the global RNG
-            push!(seed_s, seed)
+                push!(seed_s, seed)
         end
         nothing
     end)
@@ -133,13 +155,21 @@ Base.@ccallable function app()::Cint # For compilation with PackageCompiler
         srand(rng, seed) # Inits local RNG
         scatter.(rand(rng, Point3f0, n), 0, spread)
     end
-    relations_s = map(walkers_s, attrac_s, variance_s, seed_s) do n, attrac, variance, seed
-        srand(rng, seed) # Inits local RNG # scatter.(rand(rng, n, n), attrac, variance)
-        scatter.(offsetcols(eye(n)), attrac, variance)
+    relations_s = map(walkers_s, relation_s, attrac_s, variance_s, seed_s) do n, model, avg, var, seed
+        srand(rng, seed) # Inits local RNG
+        if model == onetoone
+            rel = offsetcols(eye(n))
+        elseif model == electronic
+            loads = repmat(rand(rng, n), 1, n)
+            rel = -transpose(loads) .* loads
+        else
+            rel = rand(rng, n, n)
+        end
+        scatter.(rel, avg, var)
     end
 
-    states_s = map(iterations_s, points_s, relations_s) do iters, pts, rels
-        walk(map(value, (iters, pts, rels))...)
+    states_s = map(law_s, iterations_s, points_s, relations_s) do law, iters, pts, rels
+        walk(map(value, (law, iters, pts, rels))...)
     end
     rings_s = map(states_s, iterations_s, walkers_s) do states, iters, wlkrs
         reshape(states, wlkrs, iters)
